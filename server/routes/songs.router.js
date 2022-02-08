a42dfeddb40674b67867d9b5a50d341d65503b5c
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-08 16:04:04 -0600

    Functioning

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const pg = require('pg');
+const pool = require('../modules/pool');
+// const Pool = pg.Pool;
+
+// const pool = new Pool({
+//     database: 'music_library', // Name of database
+//     host: 'localhost', // Where our database is
+//     port: '5432', // the port for your db; 5432 is default for Postgres
+//     max: 10, // How many connections (queries) at one time
+//     idleTimeoutMillis: 30000 // 30 seconds to try to connect, then cancel query
+// })
+
+// Is not required, but is useful for debugging
+// pool.on('connect', () => {
+//     console.log('PostgreSQL is connected');
+// })
+
+// the pool will emit an error on behalf of any idle clients
+// pool.on('error', (error) => {
+//     console.log('Error in Postgres Pool', error);
+// })
 
 let songs = [
     {
@@ -23,12 +44,30 @@ let songs = [
 ];
 
 router.get('/', (req, res) => {
-    res.send(songs);
+    // res.send(songs);
+    // check SQL query text in Postico first!
+    let queryText = 'SELECT * FROM "songs";';
+    pool.query(queryText).then((result) => {
+            res.send(result.rows)
+        }).catch((err) => {
+            console.log('Error making query', queryText, err);
+            res.sendStatus(500);
+        })
 });
 
 router.post('/', (req, res) => {
-    songs.push(req.body);
-    res.sendStatus(200);
+    const newSong = req.body;
+    const queryText = `
+    INSERT INTO "songs" ("artist", "track", "published", "rank")
+    VALUES ($1, $2, $3, $4);
+    `;
+    // parameterized query, prevents SQL injection
+    pool.query(queryText, [newSong.artist, newSong.track, newSong.published, newSong.rank]).then((result) => {
+        res.sendStatus(201)
+    }). catch((err) => {
+    console.log('Error Querying', queryText, err);
+        res.sendStatus(500);
+    })
 });
 
 module.exports = router;
